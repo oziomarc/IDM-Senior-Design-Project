ad97d1c72cbba2650c6810b6bf6162082dd48beb
oziomarc <oziomarc@gmail.com>
2023-03-25 17:27:31 -0400

    gallery style

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,6 +3,16 @@ import { useNavigate } from "react-router";
 import Header from "../components/Header";
 import { getFirestore, collection, getDocs } from "firebase/firestore"
 import Booth from "./Booth";
+import filler1 from "../files/filler00001.png"
+import filler2 from "../files/filler00002.png"
+import filler3 from "../files/filler00003.png"
+import filler4 from "../files/filler00004.png"
+import filler5 from "../files/filler00005.png"
+import filler6 from "../files/filler00006.png"
+import filler7 from "../files/filler00007.png"
+import filler8 from "../files/filler00008.png"
+import filler9 from "../files/filler00009.png"
+import filler10 from "../files/filler00010.png"
 
 const queryData = async (app) => {
     if(!app) return [];
@@ -52,6 +62,36 @@ function Gallery({ caption, app, capturedImage }) {
             <img key={image} src={image} alt="selfie" />
           ))} */}
         <div className="galleryWrapper">
+            <div className="galleryItem">
+              <img src={filler1} alt="filler-1" id="1433"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler7} alt="filler-7" id="1839"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler8} alt="filler-8" id="1900"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler4} alt="filler-4" id="1914"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler5} alt="filler-5" id="1966"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler9} alt="filler-9" id="1966"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler6} alt="filler-6" id="1920"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler3} alt="filler-3" id="2011"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler10} alt="filler-10" id="2014"></img>
+            </div>
+            <div className="galleryItem">
+              <img src={filler2} alt="filler-2" id="2016"></img>
+            </div>
             <div className="galleryItem">
                 {/* {uploadData.map((upload) => (
                     <img src={upload}></img>,
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import babyozi from "../files/babyozi.jpg"
 
 function Landing() {
   return (
@@ -23,9 +24,15 @@ function Landing() {
             </div>    
         </div>
         <div id="about">
+          <div>
             <p>Capturing Humanity is an interactive installation that showcases the history of humanity through self-portraits, also known as 'selfies'. The project invites passersby to take self-portraits and contribute to a gallery and timeline of self-portraits throughout history. This creates a sense of connection and identity among people, while demonstration how human behavior has evolved alongside technology.</p>
             <p>The installation also aims to evoke a vintage, nostalgic feeling that is tied to the history of image capture. This is achieved through the website UI and a thermal printer that provides participants with a keepsake portrait, similar to a photo booth.</p>  
             <p>Overall, the project places the tradition of self-portraits within a larger context of human history and identity.</p>
+          </div>
+          <div id="author">
+            <img src={babyozi}></img>
+            <p>The <a href="https://www.ozioma.xyz" target={"_blank"} >creator</a> of this website's rumored first selfie, taken on a Nintendo DSi (2011).</p>
+          </div>  
         </div>
         <div className="buttonClass">
             <Link to="/booth" style={{ textDecoration: 'none' }}>
