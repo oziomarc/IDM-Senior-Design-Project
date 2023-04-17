0ee3a13148965f00bb0cd804273c7423c0934f06
oziomarc <oziomarc@gmail.com>
2023-04-17 11:34:32 -0400

    many changes

--- src/pages/Booth.js
+++ src/pages/Booth.js
@@ -1,36 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router";
-import Header from "../components/Header";
 import Webcam from "webcam-easy";
 import { Switch, FormGroup, FormControlLabel } from '@mui/material';
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase";
+import { Link } from "react-router-dom";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-function Booth() {
+function Booth(app) {
   const navigate = useNavigate()
-   const [webcam, setWebcam] = useState(null);
-   const [active, setActive] = React.useState(true);
+  const [webcam, setWebcam] = useState(null);
+  const [active, setActive] = React.useState(true);
 
   useEffect(() => {
      const webcamElement = document.getElementById("webcam");
      const canvasElement = document.getElementById("canvas");
-     const snapSoundElement = document.getElementById("snapSound");
-
-     const webcamInstance = new Webcam(
-       webcamElement,
-       "user",
-       canvasElement,
-       snapSoundElement
-     );
-
+     const webcamInstance = new Webcam(webcamElement, "user", canvasElement);
      setWebcam(webcamInstance);
-
      return () => {
        webcamInstance.stop();
      };
    }, []);
 
-   const handlePermission = () => {
+  const handlePermission = () => {
     setActive(!active)
     if (webcam && active) {
       webcam
@@ -49,32 +41,71 @@ function Booth() {
 
   const handleCapture = () => {
     if (webcam) {
-      const picture = webcam.snap();
-      if (picture == null) return;
-      // convert base64 to png
-      const imageData = picture.split(",")[1];
-      const image = new Image();
-      image.onload = function() {
-      const canvas = document.createElement("canvas");
-      canvas.width = image.width;
-      canvas.height = image.height;
-      const context = canvas.getContext("2d");
-      context.drawImage(image, 0, 0);
-      canvas.toBlob((blob) => {
-        const file = new File([blob], "filename.png", { type: "image/png" });
-        const imageRef = ref(storage, `selfies/${Date.now()}.png`, { contentType: "image/png" });
-        uploadBytes(imageRef, file, { contentType: "image/png" }).then(() => {
-          getDownloadURL(imageRef).then((url) => {
-            console.log("image ref: "+imageRef)
-            console.log("file: "+file)
-            console.log("url: "+url)
-          });
-        });
-      }, "image/png");
-    };
-    image.src = "data:image/png;base64," + imageData;
+      const countdownElement = document.getElementById("countdownElement");
+      let countdown = 3;
+      countdownElement.innerHTML = countdown;
+      const countdownInterval = setInterval (() => {
+        countdown--
+        if (countdown === 0) {
+          clearInterval(countdownInterval);
+          countdownElement.innerHTML = "";
+          const picture = webcam.snap();
+          if (picture == null) return;
+          // convert base64 to png
+          const imageData = picture.split(",")[1];
+          const image = new Image();
+          image.onload = function() {
+            const canvas = document.createElement("canvas");
+            canvas.width = 384;
+            canvas.height = 576;
+            canvas.className = "imageCanvas"
+            const context = canvas.getContext("2d");
+            context.drawImage(image, 0, 0);
+            canvas.toBlob((blob) => {
+              const file = new File([blob], "filename.png", { type: "image/png" });
+              const imageRef = ref(storage, `selfies/${Date.now()}.png`, { contentType: "image/png" });
+              uploadBytes(imageRef, file, { contentType: "image/png" }).then(() => {
+                getDownloadURL(imageRef).then((url) => {
+                  console.log("image ref: "+imageRef)
+                  console.log("file: "+file)
+                  console.log("url: "+url)
+                });
+              });
+            }, "image/png");
+          };
+          image.src = "data:image/png;base64," + imageData;
+        } else {
+          countdownElement.innerHTML = countdown;
+        }
+      }, 1000);
+    }
   }
-}
+
+  const addCaption = () => {
+    return (
+      <>
+          <form className="captionForm" onSubmit={(e) => sendCaption(e)}>
+              <label htmlFor="captionText" style={{fontSize: 24}}>Caption your photo.</label>
+              <textarea type="text" id="galleryCaption" name="captionText" maxlength="140"></textarea>
+              <button type="submit">done</button>
+          </form>
+      </>
+    )
+  }
+
+  const sendCaption = useCallback(async (e) => {
+    e.preventDefault();
+    const db = getFirestore(app);
+    const captionText = e.currentTarget.captionText.value;
+    try {
+        const docRef = await addDoc(collection(db, `selfieCaptions/`), {
+            captionText
+        });
+        // setPostSuccessful(true)
+    } catch (e) {
+        console.error("error adding document: ", e);
+    }
+  }, [app])
 
   const addToGallery = () => {
     navigate("/gallery")
@@ -92,25 +123,45 @@ function Booth() {
   }
   return (
     <>
-      <Header />
           <div className="boothWrapper">
             <div className="boothWrapper-2">
-              <div className="permissionButton">
-                <FormGroup>
-                    <FormControlLabel control={<Switch default />} onClick={handlePermission} label="Camera Access" />
-                </FormGroup>
-              </div>
+              
               <div className="cameraCanvasWrapper">
+                
                 <video id="webcam" className="cameraStream" autoPlay playsInline width="384" height="576"></video>
-                <canvas id="canvas" width="384" height="576" className="imageCanvas"></canvas>
+                <canvas id="canvas" width="384" height="576" className=""></canvas>
               </div>
-            
-            <div className="buttons">
-             <button id="" onClick={handleCapture}>Take photo</button>
-             <button onClick={addToGallery} style={{ textDecoration: 'none' } }>Add to Gallery</button>
-             <button id="print-button" onClick={printImage}>Print</button>
+               <div className="countdowns">
+                  <h1 id="countdownElement"></h1>
+                </div>
+              <div className="booth-buttons">
+                <div className="permissionButton">
+                    <FormGroup>
+                        <FormControlLabel control={<Switch default color="default"/>} onClick={handlePermission} label="Camera" />
+                    </FormGroup>
+                </div>
+                <div className="otherButtons">
+                  <button id="" onClick={handleCapture}>Take Photo</button>
+                  <button onClick={addToGallery} style={{ textDecoration: 'none' } }>Add to Gallery</button>
+                  <button onClick={addCaption} style={{ textDecoration: 'none' } }>Caption</button>
+                  <button id="print-button" onClick={printImage}>Print</button>
+                </div>
+              </div>
+
+          </div>
+          <div className="boothRow">
+            <div className="boothRow-1">
+              <button><Link to="/"> C.H.</Link></button>
+            </div>
+            <div className="boothRow-2">
+              <div id="b-1"></div>
+              <div id="b-2"></div>
+            </div>
+            <div className="boothRow-3" id="beepyFlash">
+              <div id="b-3"></div>
             </div>
           </div>
+          
         </div>
     </>
   );
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -11,11 +11,17 @@ import filler7 from "../files/filler00007.png"
 import filler8 from "../files/filler00008.png"
 import filler9 from "../files/filler00009.png"
 import filler10 from "../files/filler00010.png"
+import filler11 from "../files/filler00011.jpg"
+import filler12 from "../files/filler00012.png"
+import filler13 from "../files/filler00013.png"
 import { storage } from "../firebase";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 function Gallery({  }) {
   const [imageList, setImageList] = useState([])
+  const [captionList, setCaptionList] = useState([])
   const storageRef = ref(storage, `selfies/`)
+  // const docRef = addDoc(collection, `selfieCaptions`)
 
   useEffect(() => {
     listAll(storageRef).then((response) => {
@@ -38,36 +44,61 @@ function Gallery({  }) {
             <div className="galleryWrapper" id="galleryWrapper">
               <div className="galleryItem">
                 <img src={filler1} alt="filler-1" id="1433"></img>
+                <p className="galleryCaption">“Portrait of a Man in a Turban” by Jan van Eyck 1433</p>
+              </div>
+              <div className="galleryItem">
+                <img src={filler11} alt="filler-11" id="1500"></img>
+                <p className="galleryCaption">Albrecht Durer's self-portrait c. 1500</p>
               </div>
               <div className="galleryItem">
                 <img src={filler7} alt="filler-7" id="1839"></img>
+                <p className="galleryCaption">Robert Cornelius daguerreotype selfie, 1839</p>
+              </div>
+              <div className="galleryItem">
+                <img src={filler12} alt="filler-12" id="1889"></img>
+                <p className="galleryCaption">Self-portrait without beard Vincent van Gogh 1889</p>
               </div>
               <div className="galleryItem">
                 <img src={filler8} alt="filler-8" id="1900"></img>
+                <p className="galleryCaption">self portrait c. 1900</p>
               </div>
               <div className="galleryItem">
                 <img src={filler4} alt="filler-4" id="1914"></img>
+                <p className="galleryCaption">self portrait c. 1914</p>
               </div>
               <div className="galleryItem">
                 <img src={filler5} alt="filler-5" id="1966"></img>
+                <p className="galleryCaption">the first selfie in space by Buzz Aldrin. 1966</p>
               </div>
               <div className="galleryItem">
-                <img src={filler9} alt="filler-9" id="1966"></img>
+                <img src={filler9} alt="filler-9" id="19662"></img>
+                <p className="galleryCaption"></p>
               </div>
               <div className="galleryItem">
                 <img src={filler6} alt="filler-6" id="1920"></img>
+                <p className="galleryCaption">self portrait c. 1920</p>
               </div>
               <div className="galleryItem">
                 <img src={filler3} alt="filler-3" id="2011"></img>
+                <p className="galleryCaption">self portrait c. 2011</p>
               </div>
               <div className="galleryItem">
                 <img src={filler10} alt="filler-10" id="2014"></img>
+                <p className="galleryCaption">self portrait c. 2014</p>
+              </div>
+              <div className="galleryItem">
+                <img src={filler13} alt="filler-13" id="2015"></img>
+                <p className="galleryCaption">Curiosity rover's self portrait at Mount Sharp, Mars, 2015</p>
               </div>
               <div className="galleryItem">
                 <img src={filler2} alt="filler-2" id="2016"></img>
+                <p className="galleryCaption">self portrait c. 2016</p>
               </div>
               {imageList.map((url) => {
-                return <div className="galleryItem"><img src={url}></img></div>
+                return <div className="galleryItem">
+                  <img src={url}></img>
+                  <p className="galleryCaption"></p>
+                  </div>
               })}
           </div>
         </div>
